8b04de2eab14637318878b56fab085d0f92081d6
Felipe Gimenez <rlolin1972@gmail.com>
2025-08-31 18:04:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp, getApps, FirebaseApp } from "firebase/app";
+import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,7 +17,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-let app;
+let app: FirebaseApp;
 if (!getApps().length) {
     app = initializeApp(firebaseConfig);
 } else {
@@ -26,5 +26,22 @@ if (!getApps().length) {
 
 const db = getFirestore(app);
 const storage = getStorage(app);
+
+// Enable persistence
+enableIndexedDbPersistence(db)
+  .catch((err) => {
+    if (err.code == 'failed-precondition') {
+      // Multiple tabs open, persistence can only be enabled
+      // in one tab at a time.
+      // ...
+      console.warn('Firestore persistence failed: failed-precondition. This can happen with multiple tabs open.');
+    } else if (err.code == 'unimplemented') {
+      // The current browser does not support all of the
+      // features required to enable persistence
+      // ...
+       console.warn('Firestore persistence failed: unimplemented. Browser does not support this feature.');
+    }
+  });
+
 
 export { app, db, storage };
