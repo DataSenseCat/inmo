319ebdf9be867a887b14449c0906f82eb92efeb1
Felipe Gimenez <rlolin1972@gmail.com>
2025-09-01 04:42:46 +0000

    porque asumes que el proyecto se llama datainmob, EL PROYECTO SE SIGUE L

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ import { getStorage, type FirebaseStorage } from "firebase/storage";
 const clientCredentials = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: "datainmob",
+    projectId: "catamarca-estates",
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
@@ -21,7 +21,7 @@ function getFirebaseApp(): FirebaseApp {
 }
 
 const app: FirebaseApp = getFirebaseApp();
-const db: Firestore = getFirestore(app);
+const db: Firestore = getFirestore(app, 'datainmob');
 const storage: FirebaseStorage = getStorage(app);
 
 export { app, db, storage };
